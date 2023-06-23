6d0e919be3f76045ae240a8289e4622abf9b8cb1
shakir201 <137536466+shakir201@users.noreply.github.com>
2023-06-24 01:39:18 +0500

    source.py commited in new branch

---
+++ source.py
@@ -0,0 +1,54 @@
+# name = [23, 40, [1,3,'source code'], [90, 21, 'numbers']]
+
+name = [23, 40, [15, 25, 'hassan', ['sufyn','software engineer']], 'source']
+# print(name[2][3])
+
+person = {
+    'name': 'Alice',
+    'age': 30,
+    'qualification': {
+    'matric': 'science',
+    'fsc': 'medical',
+    'bachelors': 'BSIT'
+    }
+}
+
+print(person['qualification'])
+
+
+# class Person:
+# 	count = 0
+
+# 	def __init__(self, name, age):
+# 		self.name = name
+# 		self.age = age
+
+# 		Person.count = Person.count + 1
+
+# 	def display(self):
+# 		print(f"My name is {self.name} and My age is {self.age}. Our head count is {self.count}") 
+
+
+# p1 = Person('Abid', 17)
+# p1.display()
+
+# p2 = Person('Sufyan', 22)
+# p2.display()
+
+# p3 = Person('Subhaan', 16)
+# p3.display()
+
+# p2.display()
+
+# p1.display()
+# def square(n):
+
+# 	if n % 2 == 0:
+
+# 		return n*n;
+# 	else:
+# 		return "Not even"
+# numbers = [5, 25, 35, 55, 85, 95, 105, 125, 135, 155, 10, 160]
+
+# for n in numbers:
+# 	print(f'square of {n} is {square(n)}')
